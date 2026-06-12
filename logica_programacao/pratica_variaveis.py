a695fbe21f29569a0880c89fc5c012d397000568
Nycolas Antônio <nycolasla13@gamil.com>
2026-06-12 15:02:21 -0300

    terminei

---
+++ logica_programacao/pratica_variaveis.py
@@ -0,0 +1,67 @@
+nome = "Nycolas"
+filme = "Lagaptor"
+personagem1 = "Keeast"
+personagem2 = "Protos"
+personagem3 = "Lagaptor"
+
+idade = 16
+ano_filme = 2026
+idade_personagem1 = 25
+idade_personagem2 = 100
+idade_personagem3 = 29
+
+altura = 1.63
+custo_filme = 1027000000000.00
+altura_personagem1 = 1.62
+altura_personagem2 = 1.90
+altura_personagem3 = 1.90
+
+tem_namorada = False
+filme_top_das_galaxias = True
+personagem1_mais_linda = True
+personagem2_esta_vivo = True
+personagem3_mais_serio_e_galan = True
+
+print(nome)
+print(filme)
+print(personagem1)
+print(personagem2)
+print(personagem3)
+print(idade)
+print(ano_filme)
+print(idade_personagem1)
+print(idade_personagem2)
+print(idade_personagem3)
+print(altura)
+print(custo_filme)
+print(altura_personagem1)
+print(altura_personagem2)
+print(altura_personagem3)
+print(tem_namorada)
+print(filme_top_das_galaxias)
+print(personagem1_mais_linda)
+print(personagem2_esta_vivo)
+print(personagem3_mais_serio_e_galan)
+
+usuario = input("Seu nome: ")
+filme = input("Nome do filme mais top das galáxias: ")
+
+ano_filme = int(input("Que ano vai ser Lançado o filme do Nycolas?: "))
+idade_personagem2 = int(input("Qual é o nivel do Protos?: "))
+
+personagem1 = float(input("Qual a porcentagem você da para a Keeast: "))
+personagem3 = float(input("Quanto o Lagaptor está devendo para a DreamWorks: "))
+
+keeast = input("A Keeast é a mais gata (S/N): ") 
+protos = input("O Protos está vivo? (S/N): ")
+lagaptor = input("O Lagaptor é o mais sério e galã? (S/N):")
+
+print(usuario)
+print(filme)
+print(ano_filme)
+print(idade_personagem2)
+print(personagem1)
+print(personagem3)
+print(keeast)
+print(protos)
+print(lagaptor)
---
+++ logica_programacao/variaveis.py
@@ -0,0 +1,9 @@
+nome = "Nycolas"
+idade = 16
+altura = 1.63
+tem_mundial = False
+
+print(nome)
+print(idade)
+print(altura)
+print(tem_mundial)
